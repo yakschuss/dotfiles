cdb1bec4108c55196ef4ad47f25a4785158ea1a6
Jack Schuss <jackschuss@gmail.com>
2017-05-14 00:19:01 -0400

    cask installs

---
+++ cask-installs.sh
@@ -0,0 +1,16 @@
+brew install cask
+
+brew cask install google-chrome
+brew cask install slack
+brew cask install evernote
+brew cask install postman
+brew cask install dash
+brew cask install spotify
+brew cask install iterm2
+brew cask install moom
+brew cask install paste
+brew cask install postgres
+brew cask install docker
+brew cask install screenhero
+brew cask install postico
+brew cask install karabiner
