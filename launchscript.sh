ef84ca6fb49ca21f3231bdc11f17db7768de82e1
Jack Schuss <jackschuss@gmail.com>
2017-05-17 09:29:08 -0400

    Add launchscript

---
+++ launchscript.sh
@@ -0,0 +1,13 @@
+
+open -a Slack.app
+open -a Google\ Chrome.app
+open -a Spotify.app
+open -a iTerm.app
+open -a Postman.app
+open -a Evernote.app
+open -a Screenhero.app
+open -a Postgres.app
+open -a Dash.app
+open -a Moom.app
+
+
